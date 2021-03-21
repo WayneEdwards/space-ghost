c149be117644a922f543c64edda006bad35a41db
Wayne Edwards <waedwards42@yahoo.com>
2021-03-21 16:34:49 -0700

    added div id's to columns. added to JavaScript.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 });
+
+function hoursTracker() {
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function() {
+        var timeBlockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(timeBlockHour, currentHour)
+
+        if (timeBlockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeclass("present");
+            $(this).removeclass("future");
+        } else if (timeBlockHour === currentHour) {
+            $(this).addClass("present");
+            $(this).removeclass("past");
+            $(this).removeClass("future");
+        } else(timeBlockHour > currentHour) {
+            $(this).addClass("future");
+            $(this).removeclass("past");
+            $(this).removeClass("present");
+        }
+
+    })
+}
